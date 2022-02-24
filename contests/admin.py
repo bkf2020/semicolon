1690fb78fdb3df46020544b3075122053e815f83
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-02-24 15:24:58 -0800

    Add contest models: Contest, ContestProblem, Registration: - Contest: stores name of contest, the start time and end time, and if a   contest is visible - ContestProblem: maps a problem to a certain contest - Registration: stores which user_id registered for which contest_id

--- contests/admin.py
+++ contests/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Contest, ContestProblem
 
-# Register your models here.
+admin.site.register(Contest)
+admin.site.register(ContestProblem)
--- contests/models.py
+++ contests/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from problemset.models import Problem
 
-# Create your models here.
+class Contest(models.Model):
+    name = models.CharField(max_length=100)
+    start_time = models.DateTimeField(default=timezone.now)
+    end_time = models.DateTimeField(default=timezone.now)
+    visible = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+
+class ContestProblem(models.Model):
+    contest = models.OneToOneField(Contest, on_delete=models.CASCADE)
+    problem = models.OneToOneField(Problem, on_delete=models.CASCADE)
+
+class Registration(models.Model):
+    user_id = models.IntegerField()
+    contest_id = models.IntegerField()
