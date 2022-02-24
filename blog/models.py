100bcd9e452e33f2a8579b4a1fdfddda59fac9f9
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-02-23 17:05:36 -0800

    Blog app: this stores annoucements for our website

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+
