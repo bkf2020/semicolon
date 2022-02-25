98890fa7154a975e14ae8ef9223c2863163f018c
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-02-24 16:33:44 -0800

    Render contest information on contest homepage

--- contests/views.py
+++ contests/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contest
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'contests/home.html')
+    contests = Contest.objects.all()
+    context = {
+        'contests': reversed(contests)
+    }
+    return render(request, 'contests/home.html', context)
