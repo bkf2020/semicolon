f51615a68d6fdf5aaea8749a82f7d2154bb9e4d4
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-02-24 17:02:30 -0800

    Show user if they registered for a contest or not

--- contests/views.py
+++ contests/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Contest
+from .models import Contest, Registration
 
 # Create your views here.
 
 def home(request):
     contests = Contest.objects.all()
+    for contest in contests:
+        contest.registered = False
+
+        user_registration = Registration.objects.filter(
+            user_id=request.user.id,
+            contest_id=contest.id
+        )
+        if(len(user_registration) > 0):
+            contest.registered = True
+
     context = {
         'contests': reversed(contests)
     }
