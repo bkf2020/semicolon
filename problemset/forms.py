341ce0f6c57e0242a01d1ff1cff0d9544e9729d6
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-02-27 13:14:43 -0800

    Remove limits for answers, wrap words in alert divs

--- problemset/forms.py
+++ problemset/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class ProblemForm(forms.Form):
-    answer = forms.IntegerField(label='Your answer (integer between 0 and 999 inclusive)', min_value=0, max_value=999)
+    answer = forms.IntegerField(label='Your answer (should be an integer)')
     # id of the form above should be 'your_answer'
