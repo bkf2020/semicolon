f8aaaa74d47e99e9a5502edd870b2ed7707d174e
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-04-10 21:54:06 -0700

    Add basic pagination on blog

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
 def home(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 5)
+    
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': Post.objects.all()
+        'page_obj': page_obj
     }
     return render(request, 'blog/home.html', context)
 
