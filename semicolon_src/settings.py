fa604e5bec2a6d2bf97cd96c76e25db9234cd096
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-04-12 21:58:25 -0700

    Update settings file to use environment variables, allowed_hosts, and static_root; add procfile

--- semicolon_src/settings.py
+++ semicolon_src/settings.py
@@ -20,14 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "notarealsecretkey"
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_VALUE')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['scmath.herokuapp.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -121,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"), 
