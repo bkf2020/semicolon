cba785c56734eca802635adfb7c7743a1d7adf95
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-04-14 21:45:21 -0700

    Fix bug in timers.js that wrongly computes number of days + hours left

--- static/js/timers.js
+++ static/js/timers.js
@@ -19,13 +19,11 @@ function updateTimers() {
 			totSeconds -= totSeconds % 60;
 			totSeconds /= 60;
 
-			var hours = totSeconds % 60;
-			totSeconds -= totSeconds % 60;
-			totSeconds /= 60;
-						
-			var days = totSeconds % 24;
+			var hours = totSeconds % 24;
 			totSeconds -= totSeconds % 24;
 			totSeconds /= 24;
+						
+			var days = totSeconds;
 
 			var timeToDisplay = days.toString() + "d " + hours.toString() + "h " + minutes.toString() + "m " + seconds.toString() + "s";
 			timers[i].innerText = timeToDisplay;
