bff16a7e49db6442fba82de86ada38d25b9c760b
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-12-17 20:18:21 -0800

    Add multiple choice problems in the models

--- problemset/models.py
+++ problemset/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.utils import timezone
 
+ANSWER_CHOICES = (
+    ('A', 'A'), ('B', 'B'), ('C', 'C'), ('D', 'D'), ('E', 'E'), ('Blank', 'Blank'),
+)
+
 class Problem(models.Model):
     name = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     visible = models.BooleanField(default=False)
+    multiple_choice = models.BooleanField(default=True)
     correct_answer = models.IntegerField(default=0)
-
+    correct_answer_choice = models.CharField(max_length=5, choices=ANSWER_CHOICES, default='A')
     def __str__(self):
         return self.name
 
