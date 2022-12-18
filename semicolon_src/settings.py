0c829925b72abd515c52a57f499f16bd9cc4bd03
bkf2020 <74793713+bkf2020@users.noreply.github.com>
2022-12-17 19:36:24 -0800

    remove django_on_heroku; heroku discontinued free tier

--- semicolon_src/settings.py
+++ semicolon_src/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,5 +132,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
-
-django_on_heroku.settings(locals())
